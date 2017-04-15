ef422cc454c44b7a0a79affc12e399cd78fa2fd3
AlexYaruki <alexyaruki@gmail.com>
2017-04-16 00:42:09 +0200

    Added internal namespaces, initial X86_MMX support

--- include/nexus.h
+++ include/nexus.h
@@ -6,6 +6,10 @@
 
 namespace nexus {
 
+    
+    // Core
+    namespace {
+    
     template<typename T, size_t len>
     struct vector {
         using elementType = T;
@@ -19,6 +23,204 @@ namespace nexus {
 
     };
 
+    }
+    
+#ifdef NEXUS_X86_MMX
+    
+    namespace x86_mmx {
+    
+    //Internal
+    namespace {
+    
+    template<typename T, typename E>
+    T ____mm_add(T v1, T v2) {
+        T result;
+        for(size_t i = 0; i < T::length; i++) {
+            result.template at<E>(i) = v1.template at<E>(i) + v2.template at<E>(i);
+        }
+        return result;
+    }
+    
+    template<typename T, typename E>
+    T ____mm_sub(T v1, T v2) {
+        T result;
+        for(size_t i = 0; i < T::length; i++) {
+            result.template at<E>(i) = v1.template at<E>(i) - v2.template at<E>(i);
+        }
+        return result;
+    }
+    
+    template<typename T, typename E>
+    T ____mm_adds(T v1, T v2) {
+        T result;
+        for(size_t i = 0; i < T::length; i++) {
+            E x = v1.template at<E>(i);
+            E y = v2.template at<E>(i);
+            if((x > 0) && (y > std::numeric_limits<E>::max - x)) { // Overflow
+                result.template at<E>(i) = std::numeric_limits<E>::max;
+            } else if (std::numeric_limits<E>::is_signed && (x < 0) && (y < std::numeric_limits<E>::min - x)) { //Underflow
+                result.template at<E>(i) = std::numeric_limits<E>::min;
+            } else {
+                result.template at<E>(i) = x + y;
+            }
+        }
+        return result;
+    }
+    
+    template <bool...> struct bool_pack;
+    template <bool... v>
+    using all_true = std::is_same<bool_pack<true, v...>, bool_pack<v..., true>>;
+    
+    template<typename E>
+    void ____m_set2(E* target) {
+        
+    }
+    
+    template<typename E, typename... Args, typename = std::enable_if<all_true<std::is_convertible<Args, E>{}...>{}>>
+    void ____m_set2(E* target, E value, Args... args) {
+        *target = value;
+        ____m_set2(target+1,args...);
+    }
+    
+    template<typename T,typename E,size_t N,typename... Args,typename = typename std::enable_if<sizeof...(Args) == N>::type>
+    T ____mm_set(Args&&... args) {
+        T result;
+        E *target = reinterpret_cast<E*>(&result);
+        ____m_set2(target,args...);
+        return result;
+    }
+    
+    template<typename T, typename E>
+    T ____mm_set1(E x) {
+        T result;
+        E* target = reinterpret_cast<E*>(&result);
+        for(int i = 0; i < sizeof(T)/sizeof(E); i++) {
+            target[i] = x;
+        }
+        return result;
+    }
+    
+    template<typename T>
+    T ____mm_setzero() {
+        T result;
+        for(int i = 0; i < sizeof(T); i++) {
+            result.template at<uint8_t>(i) = 0;
+        }
+        return result;
+    }
+    
+    template<typename T, typename E>
+    T ____mm_cmpeq(T v1, T v2) {
+        T result;
+        E* target = reinterpret_cast<E*>(&result);
+        for(int i = 0; i < sizeof(T)/sizeof(E); i++) {
+            target[i] = v1.template at<E>(i) == v1.template at<E>(i) ? std::numeric_limits<E>::max : 0;
+        }
+        return result;
+    }
+    
+    template<typename T, typename E>
+    T ____mm_cmpgt(T v1, T v2) {
+        T result;
+        E* target = reinterpret_cast<E*>(&result);
+        for(int i = 0; i < sizeof(T)/sizeof(E); i++) {
+            target[i] = v1.template at<E>(i) > v1.template at<E>(i) ? std::numeric_limits<E>::max : 0;
+        }
+        return result;
+    }
+    
+    template<typename T>
+    T ____mm_and(T v1, T v2) {
+        T result;
+        uint8_t* target = reinterpret_cast<uint8_t*>(&result);
+        for(int i = 0; i < sizeof(T)/sizeof(uint8_t); i++) {
+            target[i] = v1.template at<uint8_t>(i) & v1.template at<uint8_t>(i);
+        }
+        return result;
+    }
+    
+    template<typename T>
+    T ____mm_andnot(T v1, T v2) {
+        T result;
+        uint8_t* target = reinterpret_cast<uint8_t*>(&result);
+        for(int i = 0; i < sizeof(T)/sizeof(uint8_t); i++) {
+            target[i] = (~v1.template at<uint8_t>(i)) & v1.template at<uint8_t>(i);
+        }
+        return result;
+    }
+    
+    }
+    
+    using __int64 = int64_t;
+    using __m64 = vector<int8_t, 8>;
+        
+    const auto& _mm_add_pi8   = ____mm_add<__m64,  int8_t>;
+    const auto& _mm_add_pi16  = ____mm_add<__m64, int16_t>;
+    const auto& _mm_add_pi32  = ____mm_add<__m64, int32_t>;
+    
+    const auto& _mm_adds_pi8  = ____mm_add<__m64,  int8_t>;
+    const auto& _mm_adds_pi16 = ____mm_add<__m64, int16_t>;
+    const auto& _mm_adds_pu8  = ____mm_add<__m64, uint8_t>;
+    const auto& _mm_adds_pu16 = ____mm_add<__m64,uint16_t>;
+    
+    const auto& _m_paddb   = _mm_add_pi8;
+    const auto& _m_paddw   = _mm_add_pi16;
+    const auto& _m_paddd   = _mm_add_pi32;
+        
+    const auto& _m_paddsb  = _mm_adds_pi8;
+    const auto& _m_paddsw  = _mm_adds_pi16;
+    const auto& _m_paddusb = _mm_adds_pu8;
+    const auto& _m_paddusw = _mm_adds_pu16;
+        
+    const auto& _mm_sub_pi8   = ____mm_sub<__m64,  int8_t>;
+    const auto& _mm_sub_pi16  = ____mm_sub<__m64, int16_t>;
+    const auto& _mm_sub_pi32  = ____mm_sub<__m64, int32_t>;
+        
+    const auto& _m_psubb   = _mm_sub_pi8;
+    const auto& _m_psubw   = _mm_sub_pi16;
+    const auto& _m_psubd   = _mm_sub_pi32;
+    
+    const auto& _mm_set_pi8  = ____mm_set<__m64,  int8_t,8, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t>;
+    const auto& _mm_set_pi16 = ____mm_set<__m64, int16_t,4,int16_t,int16_t,int16_t,int16_t>;
+    const auto& _mm_set_pi32 = ____mm_set<__m64, int32_t,2,int32_t,int32_t>;
+    
+    const auto& _mm_set1_pi8  = ____mm_set1<__m64,  int8_t>;
+    const auto& _mm_set1_pi16 = ____mm_set1<__m64, int16_t>;
+    const auto& _mm_set1_pi32 = ____mm_set1<__m64, int32_t>;
+    
+    const auto& _mm_cmpeq_pi8  = ____mm_cmpeq<__m64,  int8_t>;
+    const auto& _mm_cmpeq_pi16 = ____mm_cmpeq<__m64, int16_t>;
+    const auto& _mm_cmpeq_pi32 = ____mm_cmpeq<__m64, int32_t>;
+    
+    const auto& _mm_cmpgt_pi8  = ____mm_cmpgt<__m64,  int8_t>;
+    const auto& _mm_cmpgt_pi16 = ____mm_cmpgt<__m64, int16_t>;
+    const auto& _mm_cmpgt_pi32 = ____mm_cmpgt<__m64, int32_t>;
+    
+    const auto& _mm_and_si64    = ____mm_and<__m64>;
+    const auto& _mm_andnot_si64 = ____mm_andnot<__m64>;
+    
+    
+    const auto& _m_pcmpeqb  = _mm_cmpeq_pi8;
+    const auto& _m_pcmpeqw = _mm_cmpeq_pi16;
+    const auto& _m_pcmpeqd = _mm_cmpeq_pi32;
+    
+    const auto& _m_pcmpgtb = _mm_cmpgt_pi8;
+    const auto& _m_pcmpgtw = _mm_cmpgt_pi16;
+    const auto& _m_pcmpgtd = _mm_cmpgt_pi32;
+    
+    
+    const auto& _mm_setzero_si64 = ____mm_setzero<__m64>();
+    
+    }
+    
+    using namespace x86_mmx;
+    
+#endif
+
+#ifdef NEXUS_ARM_NEON
+    
+    namespace arm_neon {
+    
     template<typename T, typename E>
     T __vdup(E elem) {
         T result;
@@ -27,12 +229,12 @@ namespace nexus {
         }
         return result;
     }
-
+    
     template<typename T, typename E>
     E __vget_lane(T v, size_t i) {
         return v.template at<E>(i);
     }
-
+    
     template<typename T, typename E>
     T __vdup_lane(T v, size_t i) {
         return __vdup<T,E>(__vget_lane<T,E>(v,i));
@@ -48,7 +250,7 @@ namespace nexus {
             }
         }
     }
-
+    
     template<typename T>
     T __vadd(T v1, T v2) {
         T result;
@@ -66,7 +268,7 @@ namespace nexus {
         }
         return result;
     }
-
+    
     template<typename T, typename R>
     R __vaddl(T v1, T v2) {
         R result;
@@ -75,7 +277,7 @@ namespace nexus {
         }
         return result;
     }
-
+    
     template<typename T>
     T __vmul(T v1, T v2) {
         T result;
@@ -84,9 +286,6 @@ namespace nexus {
         }
         return result;
     }
-
-    
-    
     
     // ARM NEON - Types - 64-bit
 
@@ -314,6 +513,11 @@ namespace nexus {
     const auto& vceqq_f32 = __vceq< float32x4_t, uint32x4_t>;
     ///////////////////////////////////////////////////////////////////////////
 
+    }
+    
+    using namespace arm_neon;
+    
+#endif
 
 }
 
--- src/nexus.cpp
+++ src/nexus.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <cstdint>
+
+#define NEXUS_X86_MMX
+#define NEXUS_ARM_NEON
+
 #include "nexus.h"
 
 using namespace nexus;
@@ -7,8 +11,12 @@ using namespace nexus;
 int main() {
     auto a = vdup_n_s32(INT32_MAX - 10);
     auto b = vdup_n_s32(11);
+    
     auto c = vaddl_s32(a,b);
     for(int i = 0; i < 2; i++) {
         std::cout << "[" << i << "]: " << vgetq_lane_s64(c,i) << std::endl;
     }
+    
+    auto x = _mm_set_pi32(1,2);
+    
 }
