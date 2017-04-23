c1dcd2cb716e4ed9000993d6298ad495cb595268
AlexYaruki <alexyaruki@gmail.com>
2017-04-23 20:17:24 +0200

    Tests for ARM_NEON_{vld1,dup}

---
+++ src_tests/arm_neon/test_vdup.cpp
@@ -0,0 +1,43 @@
+#include <iris.h>
+#include <cassert>
+
+using namespace iris;
+
+template<typename T>
+void test_vdup_n(T(*func)(typename T::elementType)) {
+	T::elementType data[T::length];
+	for (int i = 0; i < T::length; i++) {
+		data[i] = i;
+	}
+	T output = func((typename T::elementType)47);
+	for (int i = 0; i < T::length; i++) {
+		bool cmp = output.template at<typename T::elementType>(i) == (typename T::elementType)47;
+		assert(cmp);
+	}
+}
+
+int main() {
+	test_vdup_n<int8x8_t>(vdup_n_s8);
+	test_vdup_n<int16x4_t>(vdup_n_s16);
+	test_vdup_n<int32x2_t>(vdup_n_s32);
+	test_vdup_n<int64x1_t>(vdup_n_s64);
+
+	test_vdup_n<uint8x8_t>(vdup_n_u8);
+	test_vdup_n<uint16x4_t>(vdup_n_u16);
+	test_vdup_n<uint32x2_t>(vdup_n_u32);
+	test_vdup_n<uint64x1_t>(vdup_n_u64);
+
+	test_vdup_n<float32x2_t>(vdup_n_f32);
+
+	test_vdup_n<int8x16_t>(vdupq_n_s8);
+	test_vdup_n<int16x8_t>(vdupq_n_s16);
+	test_vdup_n<int32x4_t>(vdupq_n_s32);
+	test_vdup_n<int64x2_t>(vdupq_n_s64);
+
+	test_vdup_n<uint8x16_t>(vdupq_n_u8);
+	test_vdup_n<uint16x8_t>(vdupq_n_u16);
+	test_vdup_n<uint32x4_t>(vdupq_n_u32);
+	test_vdup_n<uint64x2_t>(vdupq_n_u64);
+
+	test_vdup_n<float32x4_t>(vdupq_n_f32);
+}
---
+++ src_tests/arm_neon/test_vld1.cpp
@@ -0,0 +1,43 @@
+#include <iris.h>
+#include <cassert>
+
+using namespace iris;
+
+template<typename T>
+void test_vld1(T(*func)(typename T::elementType*)) {
+	T::elementType data[T::length];
+	for (int i = 0; i < T::length; i++) {
+		data[i] = i;
+	}
+	T output = func(data);
+	for (int i = 0; i < T::length; i++) {
+		bool cmp = output.template at<typename T::elementType>(i) == data[i];
+		assert(cmp);
+	}
+}
+
+int main() {
+	test_vld1<int8x8_t>(vld1_s8);
+	test_vld1<int16x4_t>(vld1_s16);
+	test_vld1<int32x2_t>(vld1_s32);
+	test_vld1<int64x1_t>(vld1_s64);
+
+	test_vld1<uint8x8_t>(vld1_u8);
+	test_vld1<uint16x4_t>(vld1_u16);
+	test_vld1<uint32x2_t>(vld1_u32);
+	test_vld1<uint64x1_t>(vld1_u64);
+
+	test_vld1<float32x2_t>(vld1_f32);
+
+	test_vld1<int8x16_t>(vld1q_s8);
+	test_vld1<int16x8_t>(vld1q_s16);
+	test_vld1<int32x4_t>(vld1q_s32);
+	test_vld1<int64x2_t>(vld1q_s64);
+
+	test_vld1<uint8x16_t>(vld1q_u8);
+	test_vld1<uint16x8_t>(vld1q_u16);
+	test_vld1<uint32x4_t>(vld1q_u32);
+	test_vld1<uint64x2_t>(vld1q_u64);
+
+	test_vld1<float32x4_t>(vld1q_f32);
+}
