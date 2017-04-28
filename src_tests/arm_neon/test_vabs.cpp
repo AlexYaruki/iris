d86c8e0be8cf9e5f5f4571261ef22ee0af476d9c
AlexYaruki <alexyaruki@gmail.com>
2017-04-28 17:54:16 +0200

    Additional tests for ARM_NEON_vabs

--- src_tests/arm_neon/test_vabs.cpp
+++ src_tests/arm_neon/test_vabs.cpp
@@ -21,17 +21,26 @@ void test_vabs(T(*func)(T)) {
     for(size_t i = 0; i < T::length; i++) {
         assert(result.template at<typename T::elementType>(i) == static_cast<typename T::elementType>(i));
     }
+
+    for(size_t i = 0; i < T::length; i++) {
+        v.template at<typename T::elementType>(i) = std::numeric_limits<typename T::elementType>::min();
+    }
+    result = func(v);
+    for(size_t i = 0; i < T::length; i++) {
+        assert(result.template at<typename T::elementType>(i) == static_cast<typename T::elementType>(i));
+    }
 }
 
 int main() {
     test_vabs(vabs_s8);
     test_vabs(vabs_s16);
     test_vabs(vabs_s32);
+
     test_vabs(vabs_f32);
 
     test_vabs(vabsq_s8);
     test_vabs(vabsq_s16);
     test_vabs(vabsq_s32);
-    test_vabs(vabsq_f32);
 
+    test_vabs(vabsq_f32);
 }
