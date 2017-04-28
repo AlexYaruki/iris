7f54475e6d2ffb48e2be13d943c407e5b64a6602
AlexYaruki <alexyaruki@gmail.com>
2017-04-28 08:03:30 +0200

    Added tests for ARM_NEON_vhsub

---
+++ src_tests/arm_neon/test_vhsub.cpp
@@ -0,0 +1,35 @@
+#include <iris.h>
+#include <cassert>
+
+using namespace iris;
+
+template<typename T>
+void test_vhsub(T(*func)(T,T)) {
+    T v1, v2;
+    for(int i = 0; i < T::length; i++) {
+        v1.template at<typename T::elementType>(i) = i;
+        v2.template at<typename T::elementType>(i) = -i;
+    }
+    T result = func(v1,v2);
+    for(int i = 0; i < T::length; i++) {
+        assert(result.template at<typename T::elementType>(i) == i);
+    }
+}
+
+int main() {
+    test_vhsub(vhsub_s8);
+    test_vhsub(vhsub_s16);
+    test_vhsub(vhsub_s32);
+
+    test_vhsub(vhsub_u8);
+    test_vhsub(vhsub_u16);
+    test_vhsub(vhsub_u32);
+
+    test_vhsub(vhsubq_s8);
+    test_vhsub(vhsubq_s16);
+    test_vhsub(vhsubq_s32);
+
+    test_vhsub(vhsubq_u8);
+    test_vhsub(vhsubq_u16);
+    test_vhsub(vhsubq_u32);
+}
