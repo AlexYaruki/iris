baaba5126cf18632325341b25a0d2ca41be94ad6
AlexYaruki <alexyaruki@gmail.com>
2017-05-02 14:45:53 +0200

    Added implementation and tests for ARM_NEON_{vcvt,vcvt_n}

---
+++ src_tests/arm_neon/test_vcvt.cpp
@@ -0,0 +1,28 @@
+#include <iris.h>
+#include <cassert>
+
+using namespace iris;
+
+template <typename T, typename R>
+void test_vcvt(R(*func)(T)) {
+    T v;
+    for(size_t i = 0; i < T::length; i++) {
+        v.template at<typename T::elementType>(i) = i;
+    }
+    R result = func(v);
+    for(size_t i = 0; i < T::length; i++) {
+        assert(result.template at<typename R::elementType>(i) == static_cast<typename R::elementType>(static_cast<typename T::elementType>(i)));
+    }
+}
+
+int main() {
+    test_vcvt(vcvt_f32_u32);
+    test_vcvt(vcvt_f32_s32);
+    test_vcvt(vcvt_u32_f32);
+    test_vcvt(vcvt_s32_f32);
+
+    test_vcvt(vcvtq_f32_u32);
+    test_vcvt(vcvtq_f32_s32);
+    test_vcvt(vcvtq_u32_f32);
+    test_vcvt(vcvtq_s32_f32);
+}
---
+++ src_tests/arm_neon/test_vcvt_n.cpp
@@ -0,0 +1,30 @@
+#include <iris.h>
+#include <cassert>
+
+using namespace iris;
+
+template <typename T, typename R>
+void test_vcvt(R(*func)(T,int32_t)) {
+    T v;
+    for(size_t j = 0; j < T::length; j++) {
+        for(size_t i = 0; i < T::length; i++) {
+            v.template at<typename T::elementType>(i) = i;
+        }
+        R result = func(v,j);
+        for(size_t i = 0; i < T::length; i++) {
+            assert(result.template at<typename R::elementType>(i) == static_cast<typename R::elementType>(static_cast<typename T::elementType>(j)));
+        }
+    }
+}
+
+int main() {
+    test_vcvt(vcvt_n_f32_u32);
+    test_vcvt(vcvt_n_f32_s32);
+    test_vcvt(vcvt_n_u32_f32);
+    test_vcvt(vcvt_n_s32_f32);
+
+    test_vcvt(vcvtq_n_f32_u32);
+    test_vcvt(vcvtq_n_f32_s32);
+    test_vcvt(vcvtq_n_u32_f32);
+    test_vcvt(vcvtq_n_s32_f32);
+}
